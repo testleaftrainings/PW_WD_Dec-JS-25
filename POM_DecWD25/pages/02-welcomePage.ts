2bbec63dc3e27269b9b2664208638b1567f91f8f
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-02-09 23:08:31 +0530

    Delete POM_DecWD25 directory

--- POM_DecWD25/pages/02-welcomePage.ts
+++
@@ -1,9 +0,0 @@
-import { LoginPage } from "./01-loginPage";
-
-
-export class WelcomePage extends LoginPage{
-
-    async clickCRM(){ // it is going to perform the click action on CRM button
-        await this.page.locator(`//a[contains(text(),"CRM")]`).click()
-    }
-}
--- POM_DecWD25/pages/04-leadPage.ts
+++
@@ -1,9 +0,0 @@
-import { HomePage } from "./03-homePage";
-
-
-export class LeadPage extends HomePage {
-
-    async clickCreateLead(){
-        await this.page.locator(`//a[text()="Create Lead"]`).click()
-    }
-}
